bd1bf00e9721f88414e1fcf58afb9f0f4e29c4dc
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-05 20:59:54 +0530

    Create SizeOf() operator.cs

---
+++ Basic Programs/SizeOf() operator.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+    { 
+        static void Main(string[] args)
+        {	
+            Console.WriteLine("sizeof(byte)     : {0}", sizeof(byte));
+            Console.WriteLine("sizeof(sbyte)    : {0}", sizeof(sbyte));
+            Console.WriteLine("sizeof(short)    : {0}", sizeof(short));
+            Console.WriteLine("sizeof(ushort)   : {0}", sizeof(ushort));
+            Console.WriteLine("sizeof(char)     : {0}", sizeof(char));
+            Console.WriteLine("sizeof(int)      : {0}", sizeof(int));
+            Console.WriteLine("sizeof(float)    : {0}", sizeof(float));
+            Console.WriteLine("sizeof(double)   : {0}", sizeof(double));
+            Console.WriteLine("sizeof(bool)     : {0}", sizeof(bool));
+
+            Console.Read();
+        }
+    }
+
