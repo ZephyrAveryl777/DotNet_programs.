48611b71abd1587d3614067789d31c92c96d2839
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-09 17:49:57 +0530

    Create this.cs

---
+++ OOPS/this.cs
@@ -0,0 +1,22 @@
+using System;
+public class Employee{
+    public int id;
+    public string name;
+    public float salary;
+    public Employee(int i,string n,float s){
+        this.id = i;
+        this.name = n;
+        this.salary = s;
+    }
+    public void display(){
+        Console.WriteLine(id+" "+name+" "+salary);
+    }
+}
+class Test{
+    public static void Main(string[] args){
+        Employee e1 = new Employee(101,"S",90000f);
+        Employee e2 = new Employee(102,"A",90003f);
+        e1.display();
+        e2.display();
+    }
+}
