9a5e2642964fb12b47d96e497e522ceaaf2e044f
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:21:37 +0530

    Create Exp().cs

---
+++ Math/Exp().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double E = 0.0;
+
+        E = Math.Exp(2);
+
+        System.Console.WriteLine("EXP: "+E);
+    }
+}
