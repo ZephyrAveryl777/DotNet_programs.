922e1ce05a869159938e0482d803c441d4913a48
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:13:11 +0530

    Create Sinh().cs

---
+++ Math/Sinh().cs
@@ -0,0 +1,13 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double HyperSine = 0.0;
+
+        HyperSine = Math.Sinh(0.7);
+        System.Console.WriteLine("Hyperbolic sine of 0.7 is : " + HyperSine);
+    }
+}
