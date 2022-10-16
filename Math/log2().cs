3f81b81e659ae1f8929a59435e1ab04f9aa96f73
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:18:37 +0530

    Create log2().cs

---
+++ Math/log2().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double log10 = 0.0;
+
+        //Here we get logarithm of base 10 for 90.
+        log10 = Math.Log10(90);
+        Console.WriteLine("Logarithm of Base 10: " + log10);
+    }
+}
