4a845e10caac80ca13796c3e929fb3efa0163cba
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:16:28 +0530

    Create Atan().cs

---
+++ Math/Atan().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double Tan = 0.0;
+        
+        Tan = Math.Atan(0.7);
+
+        System.Console.WriteLine("Result is: "+Tan);
+    }
+}
