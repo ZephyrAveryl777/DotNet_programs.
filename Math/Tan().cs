2a4684bbfa185268aa112560f9911d03962d20d5
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:14:26 +0530

    Create Tan().cs

---
+++ Math/Tan().cs
@@ -0,0 +1,13 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double HyperTangent = 0.0;
+
+        HyperTangent = Math.Tan(0.7);
+        System.Console.WriteLine("Hyperbolic tangent of 0.7 is : " + HyperTangent);
+    }
+}
