b44e9772221e755bd8549fcbfa9f548e1429fed0
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:12:18 +0530

    Create Cosh().cs

---
+++ Math/Cosh().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double HyperbolicCosine  = 0.0;
+
+        HyperbolicCosine = Math.Cosh(0.7);
+
+        System.Console.WriteLine("Hyperbolic Cosine is: " + HyperbolicCosine);
+    }
+}
