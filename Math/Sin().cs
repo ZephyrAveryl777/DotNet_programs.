f44fb8f3d2aacdae022762482fa65e367728ee39
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:09:38 +0530

    Create Sin().cs

---
+++ Math/Sin().cs
@@ -0,0 +1,13 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double SINE = 0.0;
+
+        SINE = Math.Sin(0.7);
+        System.Console.WriteLine("SINE of 0.7 is : "+SINE);
+    }
+}
