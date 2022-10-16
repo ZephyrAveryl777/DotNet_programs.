679d032c4bc563f90577725042b28f684727291a
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:13:42 +0530

    Create Tah().cs

---
+++ Math/Tah().cs
@@ -0,0 +1,13 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double Tangent = 0.0;
+
+        Tangent = Math.Tan(0.7);
+        System.Console.WriteLine("Tangent of 0.7 is : " + Tangent);
+    }
+}
