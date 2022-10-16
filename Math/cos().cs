de1b358eeffd0a2b6a835df5c724ef0203efd143
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:10:39 +0530

    Create cos().cs

---
+++ Math/cos().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double Result  = 0.0;
+
+        Result = Math.Cos(0.7);
+        
+        System.Console.WriteLine("Result is: "+Result);
+    }
+}
