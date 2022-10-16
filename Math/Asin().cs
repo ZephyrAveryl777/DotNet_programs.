526ffe2bc570ca38522744bdcd4389d0e2ce4df2
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:15:08 +0530

    Create Asin().cs

---
+++ Math/Asin().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double Sin = 0.0;
+        
+        Sin = Math.Asin(0.7);
+
+        System.Console.WriteLine("Result is: "+Sin);
+    }
+}
