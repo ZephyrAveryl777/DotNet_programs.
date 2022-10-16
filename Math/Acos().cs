42af680641b424e0f6014ab59a7ed004e68f66d2
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2022-10-16 17:15:45 +0530

    Create Acos().cs

---
+++ Math/Acos().cs
@@ -0,0 +1,14 @@
+using System;
+
+class Sample
+{
+    //Entry point of Program
+    static public void Main()
+    {
+        double Cosine = 0.0;
+        
+        Cosine = Math.Acos(0.7);
+
+        System.Console.WriteLine("Result is: "+Cosine);
+    }
+}
